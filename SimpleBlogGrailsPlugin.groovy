23775519cd3ecf8daa3dbd8db2c86de025b42527
Burt Beckwith <burt@burtbeckwith.com>
2013-02-05 12:40:18 -0500

    added license, organization, issueManagement, and scm properties

--- SimpleBlogGrailsPlugin.groovy
+++ SimpleBlogGrailsPlugin.groovy
@@ -6,4 +6,9 @@ class SimpleBlogGrailsPlugin {
 	def title = 'Simple Blog'
 	def description = 'Adds a simple blog interface to an application'
 	def documentation = 'http://grails.org/plugin/simple-blog'
+
+	def license = 'APACHE'
+	def organization = [name: 'Grails Plugin Collective', url: 'http://github.com/gpc']
+	def issueManagement = [system: 'Github', url: 'https://github.com/gpc/grails-simple-blog/issues']
+	def scm = [url: 'https://github.com/gpc/grails-simple-blog']
 }
